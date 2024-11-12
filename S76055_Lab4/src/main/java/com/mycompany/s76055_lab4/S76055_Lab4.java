850c2a65c70b1c72f9413cbcbc389bb3e884b33a
USER <USER@172.17.174.23>
2024-11-12 17:21:25 +0800

    lab4 quest 1

---
+++ S76055_Lab4/src/main/java/com/mycompany/s76055_lab4/S76055_Lab4.java
@@ -0,0 +1,22 @@
+package labmodule4;
+// S76055 Nabiha LM4 Question 1
+import java.util.Scanner;
+public class L4q1 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+            System.out.print("Enter first integer: ");
+            int num1 = input.nextInt();
+            System.out.print("Enter second integer: ");
+            int num2 = input.nextInt();
+            int squarel = num1*num1;
+            int square2 = num2*num2;
+            int sumSquare = squarel + square2;
+            int diffSquare = squarel - square2;
+        System.out.println("The square of " + num1 +" is " + squarel);
+        System.out.println("The square of " + num2 +" is " +square2);
+        System.out.println("The sum of the squares is " +sumSquare);
+        System.out.println("The difference of the squares is " +diffSquare);
+    
+    }
+}
